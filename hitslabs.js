ecf12a5da264f9fd846c6bb39b7a2bb52c25434b
Brian Nadzan <bnadzan@hitslabs.com>
2016-03-31 14:19:17 -0400

    Initial source

--- hitslabs.js
+++ hitslabs.js
@@ -13,6 +13,7 @@ var exphbs = require("express-handlebars")({
         }
     }
 });
+var route = express.Router();
 
 //Create and configure our Express app
 var app = express();
@@ -25,39 +26,50 @@ app.use("/",express.static(__dirname + "/public"));
 
 //Root home path
 app.get("/", function(req, res) {
-    res.render("home.hbs");
+    res.render("home.hbs", {
+        home: 'class="current"',
+    });
 });
 
 //About path
 app.get("/about", function(req, res) {
-    res.render("about.hbs");
+    res.render("about.hbs", {
+            about: 'class="current"',
+    });
 });
 
 //Doctor Search path
 app.get("/Search", function(req, res) {
-    res.render("search.hbs");
+    res.render("search.hbs", {
+        search: 'class="current"',
+    });
 });
 
 //User Login path
 app.get("/login", function(req, res) {
-    res.render("login.hbs");
+    res.render("login.hbs", {
+        login: 'class="current"',
+    });
+    console.log(req.body);
 });
 
 //Contact path
 app.get("/contact", function(req, res) {
-	res.render("contact.hbs");
+	res.render("contact.hbs", {
+        contact: 'class="current"',
+    });
 });
 
 //404 page not found handler
-app.use(function(req, res) {
+app.use(function(err, req, res, next) {
     res.status(404);
     res.render("error/404.hbs");
 });
 
 //500 server error handler
-app.use(function(err, req, res) {
+app.use(function(err, req, res, next) {
     console.error(err.stack);
-    res.status(500);
+    res.status(err.status || 500);
     res.render("error/500.hbs");
 });
 
